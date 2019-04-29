2e1a38c07e25c61c1109a5c147c9589160c8c52f
Wiktor Niesiobędzki <github@vink.pl>
2019-04-29 13:58:37 +0200

    Update components before deploying to Cloud Run

--- overpass/init/startup.sh
+++ overpass/init/startup.sh
@@ -11,8 +11,8 @@ apt install -y docker.io google-cloud-sdk git
     docker run --name overpass_step1 \
         -e OVERPASS_META=yes \
         -e OVERPASS_MODE=init \
-        -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/monaco-latest.osm.bz2 \
-        -e OVERPASS_DIFF_URL=http://download.geofabrik.de/europe/monaco-updates/ \
+        -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/poland-latest.osm.bz2 \
+        -e OVERPASS_DIFF_URL=http://download.geofabrik.de/europe/poland-updates/ \
         -e OVERPASS_COMPRESSION=lz4 \
         -i \
         wiktorn/overpass-api \
@@ -26,6 +26,7 @@ apt install -y docker.io google-cloud-sdk git
     docker commit --change 'CMD /app/docker-entrypoint.sh' overpass_step1 gcr.io/osm-vink/overpass-poland:latest
     gcloud auth configure-docker -q
     gcloud docker -- push gcr.io/osm-vink/overpass-poland:latest
+    gcloud components update
     gcloud beta run deploy overpass-poland \
         --image=gcr.io/osm-vink/overpass-poland:latest \
         --region=us-central1 \
@@ -34,4 +35,3 @@ apt install -y docker.io google-cloud-sdk git
 ) | gsutil cp - gs://vink-osm-startup-scripts-us/overpass/init.log
 
 gcloud compute instances list --filter 'labels.machine_type=overpass' --uri | xargs gcloud compute instances delete --quiet
-
