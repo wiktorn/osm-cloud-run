b9609b1a54d8903039eb5d29e8a07986c52002ef
Wiktor Niesiobędzki <github@vink.pl>
2019-05-18 13:34:09 +0200

    Add comments, use stackdriver agent

--- overpass/init/startup.sh
+++ overpass/init/startup.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env sh
 
-apt update
-apt install -y docker.io google-cloud-sdk git
-
+# assume gcloud is installed
+# apt install -y google-cloud-sdk
 (
     set -e
     echo "Starting init"
     date
+    # install agent
     curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
     bash install-logging-agent.sh
+
+    # mount local SSD
+    mkfs.ext4 /dev/nvme0n1
+    mkdir /var/lib/docker
+    mount /dev/nvme0n1 /var/lib/docker/
+
+    # install git, docker
+    apt update
+    apt install -y docker.io git
+
+    # prepare image
     docker run --name overpass_step1 \
         -e OVERPASS_META=yes \
         -e OVERPASS_MODE=init \
@@ -19,20 +30,26 @@ apt install -y docker.io google-cloud-sdk git
         wiktorn/overpass-api \
         /bin/bash -c '/app/docker-entrypoint.sh'
 
+    # get modifications
     mkdir /app
     cd /app
     git clone https://github.com/wiktorn/osm-cloud-run.git
     cd osm-cloud-run/overpass/init
     docker cp supervisord.conf overpass_step1:/etc/supervisor/supervisord.conf
     docker commit --change 'CMD /app/docker-entrypoint.sh' overpass_step1 gcr.io/osm-vink/overpass-poland:latest
+
+    # upload image
     gcloud auth configure-docker -q
     gcloud docker -- push gcr.io/osm-vink/overpass-poland:latest
+
+    # deploy on Cloud Run
     gcloud beta run deploy overpass-poland \
         --image=gcr.io/osm-vink/overpass-poland:latest \
         --region=us-central1 \
         --memory=1024Mi \
         --allow-unauthenticated
 
+    # Cleanup older images
     for SHA in `gcloud container images list-tags --filter='NOT tags=latest' gcr.io/osm-vink/overpass-poland --format='get(digest)'`
     do
         gcloud container images delete --quiet gcr.io/osm-vink/overpass-poland@$SHA
@@ -40,8 +57,10 @@ apt install -y docker.io google-cloud-sdk git
     echo "Finished init"
     date
 
-) | gsutil cp - gs://vink-osm-startup-scripts-us/overpass/init.log
+) 2>1 | gsutil cp - gs://vink-osm-startup-scripts-us/overpass/init.log
 
 gcloud compute instances list --filter 'labels.machine_type=overpass' --uri | xargs gcloud compute instances delete --quiet
 
-
+#
+# trigger
+# $ gcloud pubsub topics publish osm-vink-scheduled-instances --message '{"project": "osm-vink","zone": "us-central1-c","name": "overpass-init","machine_type": "n1-standard-2","machine_label": "overpass","script_url_base": "gs://vink-osm-startup-scripts-us/overpass/init/","disk_size": "10","local_ssd": true}'
--- overpass/update/startup.sh
+++ overpass/update/startup.sh
@@ -3,9 +3,24 @@
 
 (
     set -e
+    echo "Starting update"
+    date
+
+    # install agent
+    curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+    bash install-logging-agent.sh
+
+
+    # mount local ssd
+    mkfs.ext4 /dev/nvme0n1
+    mkdir /var/lib/docker
+    mount /dev/nvme0n1 /var/lib/docker/
+
+    # install software
     apt update
-    apt install -y docker.io google-cloud-sdk git python3 python3-venv
+    apt install -y docker.io git python3 python3-venv
     gcloud auth configure-docker -q
+
     gcloud docker -- run  --name overpass_step1 \
             -e OVERPASS_META=yes \
             -e OVERPASS_MODE=init \
@@ -15,22 +30,45 @@
             -i \
             gcr.io/osm-vink/overpass-poland:latest \
             /bin/bash -c "/app/bin/update_overpass.sh"
+
     docker commit --change 'CMD /app/docker-entrypoint.sh' overpass_step1 gcr.io/osm-vink/overpass-poland:latest
     docker rm overpass_step1
+
+    # squash image
     python3 -m venv /venv
     /venv/bin/pip install docker-squash
     # FROM_LAYER=`docker images gcr.io/osm-vink/overpass-poland | grep gcr.io/osm-vink/overpass-poland | grep -v latest | awk '{print $3}'`
-    /venv/bin/docker-squash -t latest -c gcr.io/osm-vink/overpass-poland:latest
+    export DOCKER_TIMEOUT=6000
+    # /venv/bin/docker-squash -t gcr.io/osm-vink/overpass-poland:squashed -c gcr.io/osm-vink/overpass-poland:latest
+    # /venv/bin/docker-squash -c gcr.io/osm-vink/overpass-poland:latest
+    # /venv/bin/docker-squash -t squashed -c gcr.io/osm-vink/overpass-poland:latest
+    /venv/bin/docker-squash --tmp-dir /var/lib/docker/tmp/docker-squash gcr.io/osm-vink/overpass-poland:latest
+
+    NEW_IMAGE=$(docker images -a --format '{{.Repository}}:{{.ID}}' | grep -v gcr.io | awk -F ':' '{print $2}')
+    docker tag $NEW_IMAGE gcr.io/osm-vink/overpass-poland:latest
+
+    # upload image
     gcloud docker -- push gcr.io/osm-vink/overpass-poland:latest
+
+    # deploy on Cloud Run
     gcloud beta run deploy overpass-poland \
+            --async \
             --image=gcr.io/osm-vink/overpass-poland:latest \
             --region=us-central1 \
             --memory=1024Mi \
             --allow-unauthenticated
+
+    # Cleanup older images
     for SHA in `gcloud container images list-tags --filter='NOT tags=latest' gcr.io/osm-vink/overpass-poland --format='get(digest)'`
     do
         gcloud container images delete --quiet gcr.io/osm-vink/overpass-poland@$SHA
     done
-) | gsutil cp - gs://vink-osm-startup-scripts-us/overpass/update.log
+    echo "Finished update"
+    date
+
+) 2>&1 | gsutil cp - gs://vink-osm-startup-scripts-us/overpass/update.log
 
 gcloud compute instances list --filter 'labels.machine_type=overpass_update' --uri | xargs gcloud compute instances delete --quiet
+
+# trigger
+# $ gcloud pubsub topics publish osm-vink-scheduled-instances --message '{"project": "osm-vink","zone": "us-central1-c","name": "overpass-update","machine_type": "n1-highcpu-2","machine_label": "overpass_update","script_url_base": "gs://vink-osm-startup-scripts-us/overpass/update/","disk_size": "10","local_ssd": true}'
